1ddfb8114bb8cb2014969f449d402f393f8d597a
SamuelH5 <48004266+SamuelH5@users.noreply.github.com>
2020-08-27 06:12:52 +1200

    Update Login and UserInfo to use DynamoDB DOM.
    Also for #16.

--- src/main/java/secapstone/exam/Login.java
+++ src/main/java/secapstone/exam/Login.java
@@ -1,30 +1,28 @@
 package secapstone.exam;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.GetItemResult;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class Login implements RequestHandler<Map<String, String>, Map<String, String>> {
 
+	private DynamoDB dynamoDB;
+
+	public Login() {
+		dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.standard().withRegion("ap-southeast-2").build());
+	}
+
 	@Override
 	public Map<String, String> handleRequest(Map<String, String> input, Context context) {
 		Map<String, String> response = new HashMap<String, String>();
 
-		AmazonDynamoDBClientBuilder builder = AmazonDynamoDBClient.builder();
-		builder.setRegion("ap-southeast-2");
-		AmazonDynamoDB dynamoDB = builder.build();
-		GetItemResult dbResult = dynamoDB.getItem("users",
-				Collections.singletonMap("username", new AttributeValue(input.get("username"))));
-
-		if (dbResult.getItem() != null && dbResult.getItem().get("password").getS().equals(input.get("password"))) {
+		Item item = dynamoDB.getTable("users").getItem("username", input.get("username"));
+		if (item != null && item.get("password").equals(input.get("password"))) {
 			response.put("success", "true");
 		} else {
 			response.put("success", "false");
--- src/main/java/secapstone/exam/UserInfo.java
+++ src/main/java/secapstone/exam/UserInfo.java
@@ -1,31 +1,30 @@
 package secapstone.exam;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.GetItemResult;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 public class UserInfo implements RequestHandler<Map<String, String>, Map<String, String>> {
 
+	private DynamoDB dynamoDB;
+
+	public UserInfo() {
+		dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.standard().withRegion("ap-southeast-2").build());
+	}
+
 	@Override
 	public Map<String, String> handleRequest(Map<String, String> input, Context context) {
 		Map<String, String> response = new HashMap<String, String>();
 
-		AmazonDynamoDBClientBuilder builder = AmazonDynamoDBClient.builder();
-		builder.setRegion("ap-southeast-2");
-		AmazonDynamoDB dynamoDB = builder.build();
-		GetItemResult dbResult = dynamoDB.getItem("users",
-				Collections.singletonMap("username", new AttributeValue(input.get("username"))));
+		Item item = dynamoDB.getTable("users").getItem("username", input.get("username"));
 
-		if (dbResult.getItem() != null) {
-			response.put("name", dbResult.getItem().get("name").getS());
+		if (item != null) {
+			response.put("name", (String) item.get("name"));
 		}
 
 		return response;
