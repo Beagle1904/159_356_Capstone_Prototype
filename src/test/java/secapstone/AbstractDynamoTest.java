10f6aa11a3d378dbcc1585edc5a2827d7b1f02fc
Beagle1904 <54297877+Beagle1904@users.noreply.github.com>
2020-09-22 12:49:25 +1200

    Start of session lambdas
    Beginning of reformatting of project, as there are a few dead classes that aren't needed. New packages are being made to separate old classes from new ones, though old classes will be kept around for the moment.
    Closes #31

---
+++ src/test/java/secapstone/AbstractDynamoTest.java
@@ -0,0 +1,63 @@
+package secapstone;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.PrimaryKey;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.lambda.runtime.Context;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import secapstone.exam.TestContext;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AbstractDynamoTest {
+	static final protected DynamoDB DYNAMO_DB  = new DynamoDB(AmazonDynamoDBClientBuilder.standard().withRegion(Regions.AP_SOUTHEAST_2).build());
+
+	protected Context context;
+
+	private static class TableItems {
+		public String primaryKeyName;
+
+		public ArrayList<String> newItems;
+		TableItems(String pkn) {
+			primaryKeyName = pkn;
+			newItems = new ArrayList<>();
+		}
+	}
+
+	private final Map<String, TableItems> tables = new HashMap<>();
+
+	protected AbstractDynamoTest(String[] tableNames, String[] primaryKeyNames) {
+		assert (tableNames.length == primaryKeyNames.length);
+		for (int i=0; i<tableNames.length; i++) {
+			tables.put(tableNames[i], new TableItems(primaryKeyNames[i]));
+		}
+	}
+
+	protected void addItem(String tableName, String itemID) {
+		assert tables.containsKey(tableName);
+		tables.get(tableName).newItems.add(itemID);
+	}
+
+	@BeforeEach
+	void createContext() {
+		context = new TestContext();
+		((TestContext) context).setFunctionName("Login");
+	}
+
+	@AfterEach
+	void clearTables() {
+		for (String tableName : tables.keySet()) {
+			Table dbTable = DYNAMO_DB.getTable(tableName);
+			TableItems table = tables.get(tableName);
+			for (String item : table.newItems) {
+				dbTable.deleteItem(new PrimaryKey(table.primaryKeyName, item));
+			}
+			table.newItems.clear();
+		}
+	}
+}
