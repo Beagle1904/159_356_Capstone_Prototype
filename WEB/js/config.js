654cdbcff366e2304957be794e41b54118caeaa9
Joseph Shields <joseph.h.shields@gmail.com>
2020-10-07 19:12:24 +1300

    Addition of JavaScript to enable logon via AWS Cognito.

---
+++ WEB/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-2_dms9yK8VG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '126mb8rpsbhmjjchlkcdc31q9i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-southeast-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://mkwhjgbfl6.execute-api.ap-southeast-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
