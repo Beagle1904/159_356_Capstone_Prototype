210ad1049561c7ea0138e5878590e7862f039975
Beagle1904 <54297877+Beagle1904@users.noreply.github.com>
2020-10-10 21:13:51 +1300

    Created empty GetQuestion exam function and unit test class

---
+++ src/main/java/secapstone/exam/GetQuestion.java
@@ -0,0 +1,12 @@
+package secapstone.exam;
+
+import com.amazonaws.services.lambda.runtime.Context;
+
+import java.util.Map;
+
+public class GetQuestion extends AbstractExamFunction {
+	@Override
+	public Map<String, Object> handleRequest(Map<String, Object> input, Context context) {
+		return null;
+	}
+}
---
+++ src/test/java/secapstone/exam/GetQuestionTest.java
@@ -0,0 +1,9 @@
+package secapstone.exam;
+
+import secapstone.AbstractDynamoTest;
+
+class GetQuestionTest extends AbstractDynamoTest {
+	protected GetQuestionTest() {
+		super(new String[]{}, new String[]{});
+	}
+}
