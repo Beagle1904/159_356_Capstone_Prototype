75eef0bd10b43672561cb611d2f71bbda221b917
Beagle1904 <54297877+Beagle1904@users.noreply.github.com>
2020-10-16 11:12:38 +1300

    Added ability to resume in-progress exam from landing page

--- WEB/js/dashboard.js
+++ WEB/js/dashboard.js
@@ -16,6 +16,28 @@ ExamPlatform.map = ExamPlatform.map || {};
         window.location.href = '/signin.html';
     });
 
-    $(function onDocReady() {});
+    $(function onDocReady() {
+        $("#takeExam").click(loadExamData);
+    });
+
+    function loadExamData() {
+        $.ajax({
+            method: 'GET',
+            url: _config.api.invokeUrl + '/exam/summary',
+            headers: {
+                Authorization: authToken
+            },
+            data: {},
+            contentType: 'application/json',
+            success: function (result) {
+                if(result.hasOwnProperty("questions")) window.location.href = "/exam_summary.html";
+                else window.location.href = "/pre-exam_question_selection.html"
+            },
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error('Error starting exam: ', textStatus, ', Details: ', errorThrown);
+                console.error('Response: ', jqXHR.responseText);
+            }
+        });
+    }
 
 }(jQuery));
